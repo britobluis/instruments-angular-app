0dcb49c14ef4954803469df64ab3fb3d33c2e22e
Luis Brito <nottluis@gmail.com>
2019-12-02 23:22:44 -0300

    Created instrumento-detalle component

--- src/app/components/instrumento-detalle/instrumento-detalle.component.ts
+++ src/app/components/instrumento-detalle/instrumento-detalle.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Instrumento } from 'src/app/interfaces/instrumento';
+import { InstrumentosService } from 'src/app/service/instrumentos.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-instrumento-detalle',
   templateUrl: './instrumento-detalle.component.html',
   styleUrls: ['./instrumento-detalle.component.css']
 })
-export class InstrumentoDetalleComponent implements OnInit {
+export class InstrumentoDetalleComponent {
 
-  constructor() { }
+  // Inicializa un instrumento vacio
+  private instrumentoDetalle: Instrumento;
 
-  ngOnInit() {
+  // Al ser instanciado el componente en su constructor utiliza el InstrumentosService para buscar 1 instrumento por marca
+  constructor(private activatedRoute: ActivatedRoute, private instrumentoService: InstrumentosService) {
+    this.activatedRoute.params.subscribe(params => {
+      this.instrumentoDetalle = this.instrumentoService.getInstrumentoXMarca(params.marca);
+    });
   }
-
 }
