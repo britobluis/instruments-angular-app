9f44fbe630b3cf8d2345202185482bdc56c6cc48
Luis Brito <nottluis@gmail.com>
2019-12-02 23:20:14 -0300

    Created instrumento-pago-envio component

--- src/app/components/instrumento-pago-envio/instrumento-pago-envio.component.ts
+++ src/app/components/instrumento-pago-envio/instrumento-pago-envio.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { Instrumento } from 'src/app/interfaces/instrumento';
 
 @Component({
   selector: 'app-instrumento-pago-envio',
   templateUrl: './instrumento-pago-envio.component.html',
   styleUrls: ['./instrumento-pago-envio.component.css']
 })
-export class InstrumentoPagoEnvioComponent implements OnInit {
+export class InstrumentoPagoEnvioComponent {
 
-  constructor() { }
+  // Espera un instrumento desde la lista de instrumentos-venta
+  @Input() instrumentoInput: Instrumento;
 
-  ngOnInit() {
+  constructor(private router: Router) { }
+
+  // Navega hacia componente instrumento detalle pasandole la marca del instrumento
+  public verDetalle(marca: string) {
+    this.router.navigate(['/detalle', marca]);
   }
+
 
 }
