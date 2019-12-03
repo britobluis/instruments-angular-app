5336091f5556d142ec922b1f4618104d78079528
Luis Brito <nottluis@gmail.com>
2019-12-02 23:26:45 -0300

    Modified app.component and created nav-bar component

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent implements OnInit {
+export class NavBarComponent {
 
-  constructor() { }
+  mensaje = '';
 
-  ngOnInit() {
+  constructor(private router: Router) { }
+
+  buscarInstrumentos(textoBusqueda: string) {
+    if (textoBusqueda !== '') {
+      console.log(textoBusqueda);
+      this.mensaje = '';
+      this.router.navigate(['/buscar', textoBusqueda]);
+    } else {
+      this.mensaje = 'Ingrese un término de búsqueda';
+    }
   }
 
 }
