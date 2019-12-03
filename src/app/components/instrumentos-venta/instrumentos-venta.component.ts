f02e5a1f2ddbe943dd8004f08bfd813b72670b27
Luis Brito <nottluis@gmail.com>
2019-12-02 23:18:36 -0300

    Created instrumentos-venta component ts and html

--- src/app/components/instrumentos-venta/instrumentos-venta.component.ts
+++ src/app/components/instrumentos-venta/instrumentos-venta.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Instrumento } from 'src/app/interfaces/instrumento';
+import { InstrumentosService } from 'src/app/service/instrumentos.service';
 
 @Component({
   selector: 'app-instrumentos-venta',
   templateUrl: './instrumentos-venta.component.html',
   styleUrls: ['./instrumentos-venta.component.css']
 })
-export class InstrumentosVentaComponent implements OnInit {
+export class InstrumentosVentaComponent {
 
-  constructor() { }
+  instrumentos: Instrumento[] = [];
 
-  ngOnInit() {
+  // Inyecta el InstrumentosService y usa el metodo get Instrumentos service para cargar el array instrumentos
+  constructor(private instrumentoService: InstrumentosService) {
+    console.log('InstrumentosVentaComponent');
+    this.instrumentoService.getInstrumentosFromFilePHP()
+      .subscribe(instrumentosPHP => {
+        this.instrumentos = instrumentosPHP;
+      });
   }
+
 
 }
