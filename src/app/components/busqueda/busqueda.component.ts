52870f1eecb7d92f9da973a2000588b59d6da7f6
Luis Brito <nottluis@gmail.com>
2019-12-02 23:30:40 -0300

    Created busqueda component and added nav-bar component to app.component

--- src/app/components/busqueda/busqueda.component.ts
+++ src/app/components/busqueda/busqueda.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Instrumento } from 'src/app/interfaces/instrumento';
+import { ActivatedRoute } from '@angular/router';
+import { InstrumentosService } from 'src/app/service/instrumentos.service';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
   styleUrls: ['./busqueda.component.css']
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent {
 
-  constructor() { }
+  instrumentos: Instrumento[] = [];
 
-  ngOnInit() {
+  constructor(private activatedRoute: ActivatedRoute, private instrumentoService: InstrumentosService) {
+    this.activatedRoute.params.subscribe(params => {
+      this.instrumentos = this.instrumentoService.getInstrumentosXPrecio(params.termino);
+    });
   }
 
 }
